0d4e8bbeb7306beaa0a3952e7b65cb1d1f0d9948
Alexey Vishnyakov <android@zapiataia.ru>
2016-08-06 18:09:26 +0300

    Открытие файлов только поддерживаемых форматов

--- NewPPT/Converter/Wrapper.cs
+++ NewPPT/Converter/Wrapper.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using WeCastConvertor.Forms;
-using WeCastConvertor.Utils;
 
 namespace WeCastConvertor.Converter
 {
     internal class Wrapper
     {
+        public static readonly string[] SupportedFormats = {".pptx", ".ppt"};
+        //public static readonly string[] SupportedFormats = {".pptx", ".ppt", ".pdf", ".doc", ".docx"};
+
         public static Task ConvertAsync(Presentation presentation)
         {
             return Task.Run(() =>
