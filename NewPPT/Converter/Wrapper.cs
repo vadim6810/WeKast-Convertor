910ed46d80c94467e987a9815265ebc74f0d23a9
Alexey Vishnyakov <android@zapiataia.ru>
2016-08-07 16:14:24 +0300

    Переспрашивать о выходе, если запущен процесс обработки

--- NewPPT/Converter/Wrapper.cs
+++ NewPPT/Converter/Wrapper.cs
@@ -9,15 +9,16 @@ namespace WeCastConvertor.Converter
         public static readonly string[] SupportedFormats = {".pptx", ".ppt"};
         //public static readonly string[] SupportedFormats = {".pptx", ".ppt", ".pdf", ".doc", ".docx"};
 
-        public static Task ConvertAsync(Presentation presentation)
+        public static Task<Converter> ConvertAsync(Presentation presentation)
         {
             return Task.Run(() =>
             {
                 Console.WriteLine(@"Starting convert " + presentation.SourcePath);
-                Converter converter = new Converter(presentation.SourcePath);
+                var converter = new Converter(presentation.SourcePath);
                 presentation.EzsPath = converter.Convert();
                 presentation.Convert = 100;
                 Console.WriteLine(@"Presentations converted " + presentation.EzsPath);
+                return converter;
             });
         }
     }
